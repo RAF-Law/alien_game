b4608e6af21eddfe4b38a757addb6902023b24cf
“Lawrence <2857103z@student.gla.ac.uk>
2025-03-04 19:30:35 +0000

    homepage update, relocate some files

--- alien_game/urls.py
+++ alien_game/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gameApp import views
+from django.urls import include
 
 urlpatterns = [
+    path('',views.home,name="home"),
+    path('gameApp/',include('gameApp.urls')),
     path('admin/', admin.site.urls),
+
 ]
