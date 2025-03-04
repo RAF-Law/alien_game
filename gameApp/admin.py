5d7cc9c9a5f28573bfe50904a7cf13e7a4ef3bf5
jonatan <95307849+truthfulle@users.noreply.github.com>
2025-03-04 15:30:29 +0000

    admin.py - Add internal commentary

--- gameApp/admin.py
+++ gameApp/admin.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
 from .models import Weapon, Artifact, User, Game
 
+# Register Weapon Class
 @admin.register(Weapon)
 class WeaponAdmin(admin.ModelAdmin):
     list_display = ('weapon_id', 'name', 'attack_points', 'description')
     search_fields = ('name', 'description')
 
+# Register Artifact Class
 @admin.register(Artifact)
 class ArtifactAdmin(admin.ModelAdmin):
     list_display = ('artifact_id', 'name', 'description')
     search_fields = ('name', 'description')
 
+# Register Game Class
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
     list_display = ('game_id', 'player_hp', 'player_ap', 'player_speed', 'player_food', 'game_enemies_killed', 'game_day', 'game_difficulty', 'game_map')
+
+#TODO - Register User class with no issues
